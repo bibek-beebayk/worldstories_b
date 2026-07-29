f9245d48cbf7870a374b098ab2eae963dc9b4c8f
beebayk <beebayk0001@gmail.com>
2026-07-29 11:44:20 +0545

    fix: update CORS and CSRF trusted origins to include additional domains

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -1,5 +1,6 @@
 # Email backend
 import os
+
 from core.settings.base import BASE_DIR, MIDDLEWARE
 
 DEBUG = False
@@ -7,15 +8,15 @@ DEBUG = False
 ALLOWED_HOSTS = ["worldstories-b-production.up.railway.app"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://worldstories-b-production.up.railway.app", "https://worldstories-f.netlify.app", "http://localhost:8080", "http://127.0.0.1:8080",
+    "https://worldstories-b-production.up.railway.app", "https://worldstories-f.netlify.app", "http://localhost:8080", "http://127.0.0.1:8080", "https://worldstories.net", "https://www.worldstories.net"
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "https://worldstories-b-production.up.railway.app", "https://worldstories-f.netlify.app", "http://localhost:8080", "http://127.0.0.1:8080"
+    "https://worldstories-b-production.up.railway.app", "https://worldstories-f.netlify.app", "http://localhost:8080", "http://127.0.0.1:8080", "https://worldstories.net", "https://www.worldstories.net"
 ]
 
 CORS_ORIGIN_WHITELIST = [
-    "https://worldstories-b-production.up.railway.app", "https://worldstories-f.netlify.app", "http://localhost:8080", "http://127.0.0.1:8080"
+    "https://worldstories-b-production.up.railway.app", "https://worldstories-f.netlify.app", "http://localhost:8080", "http://127.0.0.1:8080", "https://worldstories.net", "https://www.worldstories.net"
 ]
 
 MIDDLEWARE += [
